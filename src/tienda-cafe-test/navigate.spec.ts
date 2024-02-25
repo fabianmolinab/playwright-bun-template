e289a084c829049a07f33f71148c3d50015a253a
fabianmolinab <molinafabian11b@gmail.com>
2024-02-25 01:59:43 -0500

    fix: add new case navigate

---
+++ src/tienda-cafe-test/navigate.spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('navigate between the different options on the page', () => {
+    const selectors = {
+        //const linkSuscripciones = '#navbarStickyDesktop'
+        xpathSuscripciones: '//*[@id="navbarStickyDesktop"]',
+    }
+
+    test('check the different subscriptions', async ({ page }) => {
+        await test.step('user enters the page "latiendadelcafe" ', async () => {
+            await page.goto('/')
+
+            const titleHomePage =
+                'El mejor café Colombiano online | La Tienda del Café'
+
+            await expect(page).toHaveTitle(titleHomePage)
+        })
+
+        await test.step('a user click on subscriptions and valid page "subscripciones"', async () => {
+            //const linkSuscripciones = '#navbarStickyDesktop'
+            const getSuscripcions = page
+                .locator(selectors.xpathSuscripciones)
+                .getByRole('link', {
+                    name: 'Suscripciones',
+                })
+            await getSuscripcions.isVisible()
+            await getSuscripcions.click()
+            await page.waitForURL('**/collections/subscripciones')
+        })
+    })
+})
